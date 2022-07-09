8829eab3fd09d2de044371e548d9c2cf26ff6272
Gautam Singh <gautam.singh@ibm.com>
2022-07-09 20:36:35 +0700

    count number of digits using recursion explain PMI(Principle of Mathematical induction)

--- src/com/gautam/RecursionNumberOfDigits.java
+++ src/com/gautam/RecursionNumberOfDigits.java
@@ -12,8 +12,8 @@ public class RecursionNumberOfDigits {
         System.out.println(result);
     }
 
-    //According to PMI(Principle of Mathematical induction)
-    //we need to work on only three simple steps
+    // According to PMI(Principle of Mathematical induction)
+    // We need to work on only three simple steps
     // 1st Step Base Name F(0) or F(1) is true
     // 2nd Step Induction Hypothesis Assume F(k ) is true where k in general k
     // 3rd Step  Induction Step if 2nd case F(K+1) is true then Ek+1 = (k+1)(k+2)/2 is true
