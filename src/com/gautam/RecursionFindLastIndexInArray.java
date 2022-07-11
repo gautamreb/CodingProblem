5bd6a258a2e91b74e26f15eaf6cca9cc8cc3f950
Gautam Singh <gautam.singh@ibm.com>
2022-07-11 11:03:01 +0700

    find the last index of element using recursion explain PMI(Principle of Mathematical induction)

--- src/com/gautam/RecursionFindLastIndexInArray.java
+++ src/com/gautam/RecursionFindLastIndexInArray.java
@@ -4,7 +4,9 @@ public class RecursionFindLastIndexInArray {
 
     public static void main(String args []){
         int [] input = {1,2,3,2,5};
-        int result =  lastIndex(input, 5, 2, 4);// array, length or array, element, i start from length-1
+      //  int result =  lastIndex(input,  2, 4);// array, length or array, element, i start from length-1
+       // int result =  lastIndex2(input, 5, 2);// array, length or array, element, i start from length-1
+         int result =  lastIndex3(input, 5, 2, 0);// array, length or array, element, i start from length-1
         System.out.println(result);
 
     }
@@ -16,10 +18,30 @@ public class RecursionFindLastIndexInArray {
     // 3rd Step  Induction Step if 2nd case F(K+1) is true then Ek+1 = (k+1)(k+2)/2 is true
 
 
-    static int lastIndex(int [] m, int n, int x, int i){
+    static int lastIndex(int [] m,  int x, int i){
         if(i == -1 ) {return -1 ; }//  Base Case
         if(m[i] == x){return i;}
-        return   lastIndex(m, n, x, i-1); //  Recursive Case
+        return   lastIndex(m,  x, i-1); //  Recursive Case
+
+    }
+
+    static int lastIndex2(int [] m, int n, int x){
+        if(n == -1 ) {return -1 ; }//  Base Case
+        if(m[n-1] == x){return n-1;}
+        return   lastIndex2(m, n-1, x); //  Recursive Case
+
+    }
+
+    static int lastIndex3(int [] m, int n, int x, int i){
+        if(i == n ) {return -1 ; }//  Base Case
+        int indexRemArr =    lastIndex3(m, n,  x,i+1); //  Recursive Case
+        if(indexRemArr == -1){
+            if(m[i] == x){return i;}
+            else {return -1;}
+
+        }else{
+            return  indexRemArr;
+        }
 
     }
 }
