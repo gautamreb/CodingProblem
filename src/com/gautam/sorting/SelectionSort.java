d46fc50b066b927b5d0ba4fbe5d08d84dace15ac
Gautam Singh <gautam.singh@ibm.com>
2022-07-12 15:52:55 +0700

    Selection sort

---
+++ src/com/gautam/sorting/SelectionSort.java
@@ -0,0 +1,38 @@
+package com.gautam.sorting;
+
+import java.util.Scanner;
+
+public class SelectionSort {
+    public static void main(String [] args){
+        Scanner myNumber = new Scanner(System.in);
+        System.out.println("Enter Number of element ");
+        int inputNumber = myNumber.nextInt();
+        System.out.println("Enter array element");
+        Scanner myArrayEle = new Scanner(System.in);
+        int [] array = new int[inputNumber];
+        for(int i =0; i<inputNumber;i++){
+            array[i]=myArrayEle.nextInt();
+        }
+
+        selectionSort(array);
+        for(int i =0; i<array.length;i++){
+            System.out.print(array[i]+" ");
+        }
+
+    }
+
+    static void selectionSort(int[] a){
+        for(int i =0; i< a.length -1;i++){
+            int index= i;
+            for(int j =i+1; j<a.length;j++){
+                if(a[j] < a[index]){
+                    index = j;
+                }
+            }
+          int smallestNumber = a[index];
+            a[index] = a[i];
+            a[i] = smallestNumber;
+
+        }
+    }
+}
