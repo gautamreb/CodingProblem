ea5bfa9fc6a6cb14d07e8fa89a3a45594dcf532f
Gautam Singh <gautam.singh@ibm.com>
2022-07-14 14:21:59 +0700

    Optimized Bubble sort

---
+++ src/com/gautam/sorting/OptimizedBubbleSort.java
@@ -0,0 +1,45 @@
+package com.gautam.sorting;
+
+import java.util.Scanner;
+
+public class OptimizedBubbleSort {
+    public static void main(String [] args){
+        Scanner myNumber = new Scanner(System.in);
+        System.out.println("Enter Number of element ");
+        int inputNumber = myNumber.nextInt();
+        System.out.println("Enter array element");
+        Scanner myArrayEle = new Scanner(System.in);
+        int [] array = new int[inputNumber];
+        for(int i =0; i<inputNumber;i++){
+            array[i]=myArrayEle.nextInt();
+        }
+
+        bubbleSort(array);
+        for(int i =0; i<array.length;i++){
+            System.out.print(array[i]+" ");
+        }
+
+    }
+
+   /* Time Complexity :    Best: ?(n) // is array is already sorted
+                           Average: ?(n^2)
+                           Worst: O(n^2)
+    Space Complexity :  O(1) */
+
+    static void bubbleSort(int[] a) {
+        for (int i = 1; i < a.length; i++) {
+            int flag = 0;
+            for (int j = 0; j < a.length -1; j++) { // this loop line run n times // if the array is already sorted than only n times
+                if (a[j] > a[j + 1]) {
+                    int tempNumber = a[j];
+                    a[j] = a[j+1];
+                    a[j+1] = tempNumber;
+                    flag = 1;
+                }
+            }
+            if(flag == 0 ){
+                break;// array is already sorted // come out of the loop
+            }
+        }
+    }
+}
