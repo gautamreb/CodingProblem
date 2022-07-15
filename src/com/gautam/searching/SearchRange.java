b8a4c3e976469b7ae800996747b133740b2ed544
Gautam Singh <gautam.singh@ibm.com>
2022-07-15 17:23:00 +0700

    Leet code problem: Find first and last position of element in sorted array
    optimized

--- src/com/gautam/searching/SearchRange.java
+++ src/com/gautam/searching/SearchRange.java
@@ -17,21 +17,40 @@ public class SearchRange {
 
     static Vector<Integer> searchRange(Vector<Integer> input, int keyValue){
         Vector<Integer> result = new Vector<>(2);
-        int firstPos = searchFirst(input, keyValue);
-        if(firstPos == -1){
-            return result;
-        }
-        int lastPos = searchLast(input, keyValue);
+       // int firstPos = searchFirst(input, keyValue);
+       // int lastPos = searchLast(input, keyValue);
+        int firstPos = getIndex(input, keyValue, true);
+        int lastPos = getIndex(input, keyValue, false);
+
         result.add(firstPos);
         result.add(lastPos);
 
         return result;
     }
 
-   /* Time Complexity :    o(log n) */
+   /* Time Complexity :    o(log n)  space Complexity : O(1)*/
+
+    static int getIndex(Vector<Integer> input, int key, boolean isFirst) {
+        int start =0; int end = input.size()-1;
+        int ans =-1;
+        while(start <= end){
+            int mid = (start) +(end-start)/2; //use this for int overflow and under flow
+            if(input.get(mid) == key){
+                ans = mid;
+                if(isFirst){
+                    end = mid-1;
+                }//search left from mid position
+                else{
+                    start=mid+1;
+                }
+            }else if(input.get(mid) > key){end = mid-1;}
+            else{start=mid+1;}
+        }
+        return ans;
+    }
 
     static int searchFirst(Vector<Integer> input, int key) {
-        int start =0; int end = input.size();
+        int start =0; int end = input.size()-1;
         int ans =-1;
         while(start <= end){
             int mid = (start) +(end-start)/2; //use this for int overflow and under flow
@@ -45,7 +64,7 @@ public class SearchRange {
     }
 
     static int searchLast(Vector<Integer> input, int key) {
-        int start =0; int end = input.size();
+        int start =0; int end = input.size()-1;
         int ans =-1;
         while(start <= end){
             int mid = (start) +(end-start)/2; //use this for int overflow and under flow
